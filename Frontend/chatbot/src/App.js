b97a3deef0b66e4fff55a079e659a9558010be3a
RajeshRk275 <rajesheswaran27599@gmail.com>
2024-10-13 03:16:26 +0530

    Initial Frontend setup

---
+++ Frontend/chatbot/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import InputBox from './Components/InputBox';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ChatBot</h1>
+      <InputBox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/chatbot/src/Components/InputBox.jsx
@@ -0,0 +1,8 @@
+export default function InputBox() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter your query!" />
+      <button>Submit</button>
+    </div>
+  );
+}
