316b6d0f696d080807c34fcd05049fe7f47d7c64
RajeshRk275 <rajesheswaran27599@gmail.com>
2024-10-14 23:41:30 +0530

    vercel.json file added in backend

---
+++ Backend/api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const connectDB = require("../config/db"); // Updated path for Vercel
+const userRoutes = require("../routes/userRoutes");
+const responseRoutes = require("../routes/responseRoutes");
+const cors = require("cors");
+
+const app = express();
+
+// Connect to the database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Parse JSON bodies
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/responses", responseRoutes);
+
+app.use(
+  "/api",
+  (req, res, next) => {
+    console.log("API Route Hit: ", req.path);
+    next();
+  },
+  userRoutes
+);
+
+module.exports = app; // Export Express app
